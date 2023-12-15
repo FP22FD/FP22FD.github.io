4329e76943ed545f516bf7751f32e6d58af3575c
FP22FD <112474910+FP22FD@users.noreply.github.com>
2023-12-15 23:24:39 +0100

    js, media query, css

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,58 @@
-let isOpen = false;
+// function to display mobile menu
 
 document.querySelector("#openMenu").addEventListener("click", function () {
-  document.querySelector("#menu").style.display = "flex";
-});
+  let hamburger = document.querySelector("#menu");
+  if (hamburger.style.display === "flex") {
+    hamburger.style.display = "";
+  } else {
+    hamburger.style.display = "flex";
+  }
+  // setTimeout(() => {
+  //   hamburger.style.display = "none";
+  // }, 2000);
 
-document.querySelector("#closeMenu").addEventListener("click", function () {
-  document.querySelector("#menu").style.display = "";
+  // hamburger.style.display = "flex";
 });
 
 // -----------------------------------------------------------------------
 
-function btnStyle() {
+// function to put active style in a header btn
+function btnActive() {
+  const navContainer = document.querySelector("#menu");
+  const navMenu = document.querySelectorAll(".btnNav");
+
+  for (let i = 0; i < navMenu.length; i++) {
+    navMenu[i].addEventListener("click", function (event) {
+      console.log("menu clicked!");
+
+      for (let i = 0; i < navMenu.length; i++) {
+        navMenu[i].classList.remove("active");
+      }
+      const btnNav = event.currentTarget;
+      btnNav.classList.add("active");
+
+      let hamburger = document.querySelector("#menu");
+      hamburger.style.display = "none";
+    });
+  }
+}
+
+btnActive();
+
+// -----------------------------------------------------------------------
+
+// function to filter project by tags
+function functionFilter() {
   const containerBtn = document.querySelector("#filter");
   const buttons = containerBtn.getElementsByClassName("btnFilter");
 
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function (event) {
-      // dobbiamo togliere la classe active al/ai button che ce l'avevano prima
-      // ma visto che non so chi ce l'aveva (e tenersi "memoria" di chi era è un lavoro "inutile")
-      // la togliamo a tutti quanti!
-      // questo dobbiamo farlo prima, perchè se lo facciamo dopo, togliamo la classe a quello che stiamo per mettere!
-
       for (let i = 0; i < buttons.length; i++) {
         buttons[i].classList.remove("active");
       }
 
-      // il button che ha generato l'evento
       const btn = event.currentTarget;
-      // gli aggiungiamo la classe 'active'
       btn.classList.add("active");
 
       const selectedTags = btn.dataset.tags;
@@ -49,4 +74,37 @@ function btnStyle() {
   }
 }
 
-btnStyle();
+functionFilter();
+
+// -----------------------------------------------------------------------
+
+// function to go up
+
+let btnTop = document.querySelector("#top");
+btnTop.addEventListener("click", goTop);
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+// const sticky = header.offsetTop;
+
+function scrollFunction() {
+  if (window.scrollY > 20) {
+    btnTop.style.display = "block";
+  } else {
+    btnTop.style.display = "none";
+  }
+}
+
+// function goTop() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
+
+function goTop() {
+  window.scrollTo(0, 0);
+}
+
+scrollFunction();
+// goTop();
